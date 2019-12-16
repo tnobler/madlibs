b0b0e86380fe4f08c273aadd9ee87cdec1adb941
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-12-16 16:00:51 -0600

    built class component for inputs container, and built an input functional component to render custom inputs

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './header';
+import Card from './card';
 
 class Home extends Component {
   render() {
     return (
       <div className='home'>
         {Header() }
+        <Card />
       </div>
     );
   }
