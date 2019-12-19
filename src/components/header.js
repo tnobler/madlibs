bd003b0001a63aa365a55968c1e6bd2acf403570
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-12-19 09:05:37 -0600

    created background header skews

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Header = (title, content) => {
+const Header = () => {
   return (
     <div className="header">
-      <div>
+      <div className="header__skew"></div>
+      <div className="header__border"></div>
         <h1>Madlibs by Tnob</h1>
         <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
-      </div>
+      
     </div>
   );
 }
